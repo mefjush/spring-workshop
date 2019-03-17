7427db86d4939fc3bdcd89ba58d76df54109d939
Mateusz Michalowski <mateusz@adhese.eu>
2019-03-17 11:47:28 +0100

    First thymeleaf endpoint

--- kantor/src/main/java/com/sda/kantor/AboutPageController.java
+++ kantor/src/main/java/com/sda/kantor/AboutPageController.java
@@ -2,11 +2,12 @@ package com.sda.kantor;
 
 import com.sda.kantor.utils.Hostname;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainPageController {
+public class AboutPageController {
 
     @GetMapping("/about")
     @ResponseBody
@@ -14,5 +15,12 @@ public class MainPageController {
         String author = "Mateusza";
         String hostname = Hostname.getHostname();
         return "Strona napisania przez " + author + ". Uruchomiona na " + hostname + ".";
+    }
+
+    @GetMapping("/abt")
+    public String abt(Model model) {
+        model.addAttribute("name", "Mateusza");
+        model.addAttribute("host", Hostname.getHostname());
+        return "about";
     }
 }
