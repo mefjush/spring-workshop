96a19ec4182e53108cecba2cd457e8e3243da921
Mateusz Michalowski <mateusz@adhese.eu>
2019-03-24 11:58:16 +0100

    Clear imports

--- kantor/src/test/java/com/sda/kantor/KantorApplicationTests.java
+++ kantor/src/test/java/com/sda/kantor/KantorApplicationTests.java
@@ -7,10 +7,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
